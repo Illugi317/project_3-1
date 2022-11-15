839ddf959453906fa5defd340e8c9175bc723443
isayj <71944396+isayj@users.noreply.github.com>
2022-11-15 20:44:49 +0100

    changed log to csv writing in server

--- server.py
+++ server.py
@@ -6,5 +6,5 @@ while True:
     recvSize = 1024
     message, address = server_socket.recvfrom(recvSize)
     message = str(message)
-    with open('logfile.log','a') as f:
+    with open('smth.csv', 'a') as f:
         f.write(message[2:-1] + "\n")
