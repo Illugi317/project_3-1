44665d7c77ab3448fdae9f5e0ca1307a37942c9d
stijnvdgroenendaal <45199010+stijnvdgroenendaal@users.noreply.github.com>
2023-01-17 15:53:10 +0100

    Added clause when height is negative

--- visualization/distance.py
+++ visualization/distance.py
@@ -18,13 +18,13 @@ def throw_distance(df, index_max, time_of_flight):
     initial_vel = get_init_vel(y, x)
     angle = getangle(df, index_max)
     grav = 9.80665
-    #time_of_flight = time_of_flight/1000
     #height = (initial_vel * math.sin(angle)) * time_of_flight + (0.5 * grav * time_of_flight**2)
-    height = (time_of_flight*(time_of_flight * grav - 2*initial_vel*math.sin(angle)))/2
+    height = get_height(df, index_max, time_of_flight)
+    if height < 0:
+        angle = math.pi - angle
+        height = math.sqrt(height**2)
     distance = initial_vel * math.cos(angle)*(initial_vel*math.sin(angle) + math.sqrt((initial_vel * math.sin(angle))**2)+2*grav*height)
     distance = distance/grav
-    print("tof ", time_of_flight)
-    print("vel ", initial_vel)
     #distance = (initial_vel * math.cos(angle) * time_of_flight) + (0.5 * grav * grav * time_of_flight**2)
     return distance
 
@@ -33,6 +33,14 @@ def throw_distance(df, index_max, time_of_flight):
     :param index: index of throw at maximum acceleration
     :return:
 """
+def get_height(df,index_max, time_of_flight):
+    y, x = get_accel(df, index_max)
+    initial_vel = get_init_vel(y, x)
+    angle = getangle(df, index_max)
+    grav = 9.80665
+    height = (time_of_flight * (time_of_flight * grav - 2 * initial_vel * math.sin(angle))) / 2
+    return height
+
 def getangle(df, index):
     x, y, z = get_linear_xyz(df, index)
     h = math.sqrt(x**2+y**2)
@@ -64,7 +72,6 @@ def get_accel(df, index2):
 
         k = math.sqrt(df.accx[index1+x-1]**2+df.accy[index1+x-1]**2+df.accz[index1+x-1]**2)-9.80665
         acc.append(k)
-        # time value is currently at 0.1 which mean the IMU would send the data 0.1 seconds apart, change this value if you have a more accurate time stamp.
         to_seconds = df.ms[index1+x-1]
         to_seconds = to_seconds/1000
         x_counter = x_counter + to_seconds
@@ -82,11 +89,10 @@ def get_start_throw(df, index_max):
     acc = []
     x = []
     index_start = index_max -1
-    g = -9.8
+    g = -10
     x_counter = 0.00
     vel_dif = 1000
     vel_old = 0
-    print(acc)
     # Iterate through the previous indexes
     while index_start > 0:
         if ((df.accx[index_start] > g and df.accx[index_start-1] <= g) or (df.accx[index_start] < -g and df.accx[index_start-1] >= -g)) or ((df.accy[index_start] > g and df.accy[index_start-1] <= g) or (df.accy[index_start] < -g and df.accy[index_start-1] >= -g)) or ((df.accz[index_start] > g and df.accz[index_start-1] <= g) or (df.accz[index_start] < -g and df.accz[index_start-1] >= -g)):
@@ -98,7 +104,7 @@ def get_start_throw(df, index_max):
         to_seconds = to_seconds / 1000
         x_counter = x_counter + to_seconds
         x.append(x_counter)
-        if vel_dif <= 0.00001 and index_start + 3 < index_max:
+        if vel_dif <= 0.00000001 and index_start + 3 < index_max:
             print("start ", index_start)
             print("end ", index_max)
             return index_start
